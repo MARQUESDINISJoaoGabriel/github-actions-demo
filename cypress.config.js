68912a158dc91bd8cd540a1180ab5b1420c9966a
MARQUESDINISJoaoGabriel <joao-gabriel.marques-dinis@efrei.net>
2026-04-01 11:23:50 +0200

    config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   allowCypressEnv: false,
 
   e2e: {
+    specPattern: "cypress/tests/**/*.spec.js", // adjust path & pattern
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
